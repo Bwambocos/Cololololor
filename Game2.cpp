cb5b99a58feca8ea71d6b34ed6717356765f6ea1
Bwambocos <mijyumaru2525@outlook.jp>
2017-02-17 16:05:34 +0900

    Game2: 初期化部追加
    ・LevelとStageの管理を実装

--- Game2.cpp
+++ Game2.cpp
@@ -26,19 +26,28 @@ struct ball	// 球1つ
 ball ColorBall[LEVEL_MAX * 5];
 int G_Main;
 int S_BGM;
+int F_Main;
+int Score_num, Stage_num, Level_num;
 int ansBall;
 
 // 初期化（一回のみ）
 void Game2_Init1()
 {
+	Score_num = 0, Stage_num = 0, Level_num = 1;
 	G_Main = LoadGraph("data\\graph\\Game2_Main.png");
 	S_BGM = LoadSoundMem("data\\sound\\bgm.mp3");
+	F_Main = CreateFontToHandle("Meiryo UI", 32, 5, DX_FONTTYPE_ANTIALIASING_8X8);
 }
 
 // 初期化（毎回）
 void Game2_Init2()
 {
-
+	++Stage_num;
+	if (Stage_num >= STAGE_MAX)
+	{
+		++Level_num, Stage_num = 0;
+		if (Level_num >= LEVEL_MAX) { Level_num = 1; }
+	}
 }
 
 // 更新
@@ -52,4 +61,8 @@ void Game2_Draw()
 {
 	// 画像
 	DrawGraph(0, 0, G_Main, TRUE);
+
+	// 文字
+	DrawFormatStringToHandle(45, 410, GetColor(0, 0, 0), F_Main, "LEVEL %d", Level_num);
+	DrawFormatStringToHandle(450, 410, GetColor(0, 0, 0), F_Main, "STAGE %d", Stage_num);
 }
